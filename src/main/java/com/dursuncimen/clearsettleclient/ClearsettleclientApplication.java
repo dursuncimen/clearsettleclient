1ad694645bdd324749c4c5031690d8098c15022e
dursuncimen <dursun.gsate@gmail.com>
2017-06-11 14:41:54 +0300

    Created Project

---
+++ src/main/java/com/dursuncimen/clearsettleclient/ClearsettleclientApplication.java
@@ -0,0 +1,12 @@
+package com.dursuncimen.clearsettleclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearsettleclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearsettleclientApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dursuncimen/clearsettleclient/ClearsettleclientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dursuncimen.clearsettleclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClearsettleclientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
