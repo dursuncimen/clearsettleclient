ba5b371a25f63442ad784f1b3ec22c211438656f
dursuncimen <dursun.gsate@gmail.com>
2017-06-11 17:28:22 +0300

    Enabled async and add restTeamplate builder

--- src/main/java/com/dursuncimen/clearsettleclient/ClearsettleclientApplication.java
+++ src/main/java/com/dursuncimen/clearsettleclient/ClearsettleclientApplication.java
@@ -1,12 +1,36 @@
 package com.dursuncimen.clearsettleclient;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableAsync
 public class ClearsettleclientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClearsettleclientApplication.class, args);
+	}
+	
+	@Bean
+	public Executor asyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(3);
+		executor.setMaxPoolSize(5);
+		executor.setQueueCapacity(250);
+		executor.setThreadNamePrefix("Clearsettle-Async-");
+		executor.initialize();
+		return executor;
+	}
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 }
